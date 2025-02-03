675a0bd02b83447175b1559a815e5584b697a98a
faeze <faezeh.am4212@gmail.com>
2025-02-03 09:14:40 +0330

    addBoxShadowAndColorToTaildwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,32 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        styleShadowCircle:
+          "-5px -5px 5px hsla(0, 0%, 100%, .1), 10px 10px 10px rgba(0, 0, 0, .4), inset -5px -5px 5px hsla(0, 0%, 100%, .2), inset 10px 10px 10px rgba(0, 0, 0, .4)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
         darckBg: "rgb(30,35,54)",
-        tGrayAll: "rgb(169,177,214)",//#aab2d7
+        tGrayAll: "rgb(169,177,214)",
         borderDarck: "rgb(27,30,46)",
+        textActive: "rgb(61,185,201)",
+        textGreen: "rgb(126,231,135)",
+        textLightRed: "rgb(255,162,139)",
+        textPurpLeLight: "rgb(147,154,255)",
+        textYelowLight: "rgb(255,220,139)",
+        colorInputB:"rgb(96,165,250)",
+        animation: {
+          rotate: "rotateAnimation 1000ms infinite",
+        },
+        keyframes: {
+          rotateAnimation: {
+            "0%": { transform: "rotate(0deg)" },
+            "50%": { transform: "rotate(45deg)" },
+            "100%": { transform: "rotate(90deg)" },
+          },
+        },
       },
     },
   },
