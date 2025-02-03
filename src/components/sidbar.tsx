68bcbf84b67bfba821bbeb7bc4e9a56e7922dabb
faeze <faezeh.am4212@gmail.com>
2025-02-03 09:25:02 +0330

    changeSidbar

--- src/components/sidbar.tsx
+++ src/components/sidbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import FileIcon from "./icons/FileIcon";
 import SearchIcon from "./icons/SearchIcon";
 import BranchIcon from "./icons/BranchIcon";
@@ -6,38 +7,100 @@ import RunitemIcon from "./icons/RunitemIcon";
 import ExtentionitemIcon from "./icons/ExtentionitemIcon";
 import UserCircleIcon from "./icons/UserCircleIcon";
 import SettingIcon from "./icons/SettingIcon";
-import MenuCircleVerticalIcon from "./icons/MenuCircleVerticalIcon";
+import DetailSubSidbar from "./detailSubSidbar";
 
 function Sidbar() {
+  const [isFile, setIsFile] = useState(true);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isBranch, setIsBranch] = useState(false);
+  const [isRunitem, setRunitem] = useState(false);
+  const [isExtentionitem, setIsExtentionitem] = useState(false);
+
   return (
-    <div className="sidbar grid grid-cols-8 gap-2 col-span-3">
+    <div className="sidbar grid grid-cols-8 col-span-3 ">
       <div className="subSidbar flex flex-col justify-between col-span-1">
         <div className="partView flex flex-col">
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50 border-l-2">
+          <button
+            onClick={() => {
+              setIsExtentionitem(false);
+              setIsBranch(false);
+              setIsFile(true);
+              setIsSearch(false);
+              setRunitem(false);
+            }}
+            className={`p-3 text-gray-500 hover:text-tGrayAll  ${
+              isFile ? "border-l-2" : ""
+            }`}
+          >
             <FileIcon
+              className=""
               width="32"
               height="32"
             />
           </button>
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button
+            onClick={() => {
+              setIsExtentionitem(false);
+              setIsBranch(false);
+              setIsFile(false);
+              setIsSearch(true);
+              setRunitem(false);
+            }}
+            className={`p-3 text-gray-500 hover:text-tGrayAll  ${
+              isSearch ? "border-l-2" : ""
+            }`}
+          >
             <SearchIcon
               width="32"
               height="32"
             />
           </button>
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button
+            onClick={() => {
+              setIsExtentionitem(false);
+              setIsBranch(true);
+              setIsFile(false);
+              setIsSearch(false);
+              setRunitem(false);
+            }}
+            className={`p-3 text-gray-500 hover:text-tGrayAll  ${
+              isBranch ? "border-l-2" : ""
+            }`}
+          >
             <BranchIcon
               width="32"
               height="32"
             />
           </button>
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button
+            onClick={() => {
+              setIsExtentionitem(false);
+              setIsBranch(false);
+              setIsFile(false);
+              setIsSearch(false);
+              setRunitem(true);
+            }}
+            className={`p-3 text-gray-500 hover:text-tGrayAll  ${
+              isRunitem ? "border-l-2" : ""
+            }`}
+          >
             <RunitemIcon
               width="32"
               height="32"
             />
           </button>
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button
+            onClick={() => {
+              setIsExtentionitem(true);
+              setIsBranch(false);
+              setIsFile(false);
+              setIsSearch(false);
+              setRunitem(false);
+            }}
+            className={`p-3 text-gray-500 hover:text-tGrayAll  ${
+              isExtentionitem ? "border-l-2" : ""
+            }`}
+          >
             <ExtentionitemIcon
               width="32"
               height="32"
@@ -46,13 +109,13 @@ function Sidbar() {
         </div>
 
         <div className="partSetting flex flex-col">
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button className="p-3 text-gray-500 hover:text-tGrayAll ">
             <UserCircleIcon
               width="32"
               height="32"
             />
           </button>
-          <button className="p-3 hover:text-opacity-90 hover:text-gray-50">
+          <button className="p-3 text-gray-500 hover:text-tGrayAll ">
             <SettingIcon
               width="32"
               height="32"
@@ -60,19 +123,14 @@ function Sidbar() {
           </button>
         </div>
       </div>
-      <div className="detailSubSidbar col-span-7 border-r border-r-borderDarck">
-        <div className="flex justify-between py-3 pl-5 pr-3 ">
-          <span className="text-xs">EXPLORER</span>
-          <button>
-            <MenuCircleVerticalIcon
-              width="16"
-              height="16"
-            />
-          </button>
-        </div>
+      <div className="col-span-7 ml-4 border-r border-r-borderDarck">
+        {isFile && <DetailSubSidbar />}
       </div>
     </div>
   );
 }
 
 export default Sidbar;
+function useRef(arg0: null) {
+  throw new Error("Function not implemented.");
+}
