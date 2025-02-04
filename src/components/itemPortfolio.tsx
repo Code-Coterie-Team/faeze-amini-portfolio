07ca836a6eb2c81e96a2fcdf534126496fd3bc69
faeze <faezeh.am4212@gmail.com>
2025-02-04 23:38:40 +0330

    createLinkTitlePageAbout

--- src/components/itemPortfolio.tsx
+++ src/components/itemPortfolio.tsx
@@ -14,12 +14,20 @@ import TsconfigIcon from "./icons/TsconfigIcon";
 import DirectionDown01Icon from "./icons/DirectionDown01Icon";
 import StarIcon from "./icons/StarIcon";
 import Link from "next/link";
+import UserCheckIcon from "./icons/UserCheckIcon";
+import LaptopIcon from "./icons/LaptopIcon";
+import SkillsIcon from "./icons/SkillsIcon";
+import IdeaIcon from "./icons/IdeaIcon";
+import MailIcon from "./icons/MailIcom";
+import FileWorkIcon from "./icons/FileWorkIcon";
+import FileLeetcodeIcon from "./icons/FileLeetcodeIcon";
+import NextIcon from "./icons/NextIcon";
 
 function ItemPortfolio() {
   const [isPublic, setIsPublic] = useState(false);
 
   return (
-    <div className="itemPortfolio">
+    <div className="itemPortfolio overflow-y-auto">
       <div className="next pl-4 pr-1 hover:bg-borderDarck">
         <button className="flex gap-1 py-[2px] items-center">
           <DirectionRight01Icon
@@ -72,7 +80,7 @@ function ItemPortfolio() {
           <p className="text-base pl-1">public</p>
         </button>
         <Link href="/">
-          <div className="aboutme hover:bg-borderDarck flex items-center gap-2 pl-10 py-[2px]">
+          <div className="aboutmePart hover:bg-borderDarck flex items-center gap-2 pl-10 py-[2px]">
             <StarIcon
               width="18"
               height="18"
@@ -80,20 +88,106 @@ function ItemPortfolio() {
             <p className="text-base">about_me.ts</p>
           </div>
         </Link>
+        <Link href="#aboutme">
+          <div className="aboutme hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]">
+            <UserCheckIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base">About Me</p>
+          </div>
+        </Link>
+        <Link href="#workExperience">
+          <div className="workExperience hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]">
+            <LaptopIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base">Work Experience</p>
+          </div>
+        </Link>
+        <Link href="#skills">
+          <div className="skills hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]">
+            <SkillsIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base">Skills</p>
+          </div>
+        </Link>
+        <Link href="#myWork">
+          <div className="myWork hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]">
+            <IdeaIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base">My Work</p>
+          </div>
+        </Link>
+        <Link href="#contactMe">
+          <div className="contactMe hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]">
+            <MailIcon
+              className="text-blue-600"
+              width="18"
+              height="18"
+            />
+            <p className="text-base">Contact Me</p>
+          </div>
+        </Link>
       </div>
-      <div className="src pl-4 pr-1 hover:bg-borderDarck">
-        <button className="flex gap-1 items-center py-[2px]">
-          <DirectionRight01Icon
-            width="18"
-            height="18"
-          />
+      <div className="src ">
+        <div className="pl-4 pr-1">
+          <button className="flex gap-1 items-center py-[2px]hover:bg-borderDarck">
+            <DirectionRight01Icon
+              width="18"
+              height="18"
+            />
 
-          <FileSrcIcon
-            width="18"
-            height="18"
-          />
-          <p className="text-base pl-1">src</p>
-        </button>
+            <FileSrcIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base pl-1">src</p>
+          </button>
+        </div>
+
+        <div className="myWorkFolder pl-4">
+          <button className="flex gap-1 items-center py-[2px]">
+            <DirectionRight01Icon
+              width="18"
+              height="18"
+            />
+
+            <FileWorkIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base pl-1">my work</p>
+          </button>
+          <Link href="#aboutme">
+            <div className="aboutme hover:bg-borderDarck flex items-center gap-2 pl-10 py-[2px]">
+              <NextIcon
+                width="18"
+                height="18"
+              />
+              <p className="text-base">Realtor Simplified</p>
+            </div>
+          </Link>
+        </div>
+        <div className="leetcodeFolder pl-4">
+          <button className="flex gap-1 items-center py-[2px]">
+            <DirectionRight01Icon
+              width="18"
+              height="18"
+            />
+
+            <FileLeetcodeIcon
+              width="18"
+              height="18"
+            />
+            <p className="text-base pl-1">leetcode</p>
+          </button>
+        </div>
       </div>
       <div className="pl-9 pr-1 hover:bg-borderDarck">
         <button className="flex gap-1 items-center py-[2px]">
