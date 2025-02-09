15ae5258b8859c2415cbdf5394d3ddd1220b5c98
faeze <faezeh.am4212@gmail.com>
2025-02-10 02:08:14 +0330

    changePageAboutHeaderMain

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -5,18 +5,14 @@ import MyWork from "./myWork";
 import ContactMe from "./contactMe";
 import PersonalInformation from "./personalInformation";
 
-
 function AboutMe() {
   return (
-    <div className="h-full overflow-hidden">
-      <HeaderMain />
-      <div className="px-12 flex flex-col gap-80 h-full overflow-y-auto">
-        <PersonalInformation />
-        <WorkExperience />
-        <Skills />
-        <MyWork />
-        <ContactMe />
-      </div>
+    <div className="px-12 flex flex-col gap-80 h-full overflow-y-auto">
+      <PersonalInformation />
+      <WorkExperience />
+      <Skills />
+      <MyWork />
+      <ContactMe />
     </div>
   );
 }
