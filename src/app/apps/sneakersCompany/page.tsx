18c4051999ea568b61fd4b50b51f23279d20d77e
faeze <faezeh.am4212@gmail.com>
2025-02-12 01:29:46 +0330

    useComponent

--- src/app/apps/sneakersCompany/page.tsx
+++ src/app/apps/sneakersCompany/page.tsx
@@ -1,5 +1,9 @@
+import AboutProject from "@/components/aboutProject";
+import ChallengeProject from "@/components/challengeProject";
 import ArrowRightIcon from "@/components/icons/ArrowRightIcon";
 import SectionSeparator from "@/components/sectionSeparator";
+import SolutionProject from "@/components/solutionProject";
+import TechnologiesProject from "@/components/technologiesProject";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -38,139 +42,42 @@ function page() {
       name: "Git",
       image: "/Image/git-logo.webp",
     },
-  
   ];
 
   return (
     <div className="h-full overflow-y-auto">
       <div className="flex flex-col gap-9">
-        <div
-          id="about"
-          className="about"
-        >
-          <div className=" px-4 py-6">
-            <div className="flex flex-col justify-center items-center mt-24 gap-4">
-              <p className=" text-base font-semibold text-textActive">
-                Case Study
-              </p>
-              <h1 className="text-6xl text-gray-50 font-semibold pt-3 tracking-tight">
-                Sneakers Company
-              </h1>
-              <p className="text-xl max-w-3xl text-center pt-2">
-                This platform is designed to make your shopping experience
-                easier and more enjoyable.
-              </p>
-            </div>
-          </div>
+        <AboutProject
+          title={" Sneakers Company"}
+          description={
+            "This platform is designed to make your shopping experience easier and more enjoyable."
+          }
+          industry={"Web Store"}
+          year={"2024"}
+          service={"Web application"}
+          imageSrc={"/Image/sneakers-company-project01.png"}
+          imageAlt={"imageSneakersCompany"}
+        />
 
-          <div className="grid grid-cols-3 border border-tGrayAll/30 ">
-            <div className="flex flex-col  px-6 py-4 border-r border-r-tGrayAll/30">
-              <span className="text-textActive font-semibold">Industry</span>
-              <span>Web Store </span>
-            </div>
-            <div className="flex flex-col px-6 py-4 border-r border-r-tGrayAll/30">
-              <span className="text-textActive font-semibold">Year</span>
-              <span>2024</span>
-            </div>
-            <div className="flex flex-col px-6 py-4">
-              <span className="text-textActive font-semibold">Service</span>
-              <span>Web application</span>
-            </div>
-          </div>
-          <div className="p-24 w-full h-full flex justify-center items-center bg-colotBgProject bg-backgroundImgProject">
-            <Image
-              className="w-full h-full"
-              src={"/Image/sneakers-company-project01.png"}
-              alt={"imageSneakersCompany"}
-              width={144}
-              height={955}
-            ></Image>
-          </div>
-        </div>
-
-        <div
-          id="challenge"
-          className="challenge px-14 py-6"
-        >
-          <SectionSeparator />
-          <h1 className="mt-8 mb-5 font-medium text-4xl text-gray-50 tracking-tight ">
-            Challenge
-          </h1>
-          <p className=" text-lg">
-            This platform displays all the information related to the product.
-            You can select the quantity of orders and add them to your shopping
-            cart.
-          </p>
-
-          <ul className="pl-10 list-disc ">
-            <li className="mt-5 lg:text-lg">
-              Implementing Redux for displaying the website in both dark mode
-              and light mode.
-            </li>
-            <li className="mt-5 lg:text-lg">
-              Displaying the number of products available in the shopping cart,
+        <ChallengeProject
+          descrption={
+            " This platform displays all the information related to the product. You can select the quantity of orders and add them to your shopping cart."
+          }
+          item01=" Implementing Redux for displaying the website in both dark mode
+              and light mode."
+          item02="Displaying the number of products available in the shopping cart,
               along with options to increase or decrease the quantity of each
-              item.
-            </li>
+              item."
+          item03="Showcasing images for each product."
+        />
+        <SolutionProject
+          text01={"example"}
+          titleText={"Delete Application (system triggered)"}
+          text02={"example"}
+          href={"/"}
+        />
 
-            <li className="mt-5 lg:text-lg">
-              Showcasing images for each product.
-            </li>
-          </ul>
-        </div>
-        <div
-          id="solution"
-          className="solution px-14 py-6"
-        >
-          <SectionSeparator />
-          <h1 className="mt-8 mb-5 font-medium text-4xl text-gray-50 tracking-tight ">
-            Solution
-          </h1>
-          <p className="text-lg">..........</p>
-          <div className="mt-5">
-            <span className="text-lg text-textActive">
-              Delete Application (system triggered){" "}
-            </span>
-            <span>..........</span>
-          </div>
-          <div className="inline-block mt-8">
-            <div className="bg-gray-50 text-gray-950 px-3 py-1 rounded-full hover:opacity-80 flex gap-2">
-              <Link href={"/#contactMe"}>Visit website</Link>
-              <ArrowRightIcon
-                width="24"
-                height="24"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div
-          id="technologies"
-          className="technologies  px-14 py-6 mb-48"
-        >
-          <div>
-            <SectionSeparator />
-            <h1 className="mt-8 mb-5 font-medium text-4xl text-gray-50 tracking-tight ">
-              Technologies
-            </h1>
-          </div>
-          <div className="flex gap-4 mt-6 flex-wrap">
-            {technologies.map((technology, index) => (
-              <div className="h-[115px] flex flex-col items-center">
-                <Image
-                  className="w-[64px] h-[64px] "
-                  src={technology.image}
-                  alt=""
-                  width={144}
-                  height={144}
-                />
-                <h4 className="mt-2 px-2 font-medium tracking-tight text-lg text-gray-50 ">
-                  {technology.name}
-                </h4>
-              </div>
-            ))}
-          </div>
-        </div>
+        <TechnologiesProject listTechnologies={technologies} />
       </div>
     </div>
   );
