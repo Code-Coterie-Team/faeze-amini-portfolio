7b2720ff982c7a8e0b5d7c9599094499217a5632
faeze <faezeh.am4212@gmail.com>
2025-02-11 10:45:23 +0330

    createComponentPakageFile

---
+++ src/components/pakageFile.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+
+interface PakageFiles {
+
+  IconName: ReactNode;
+  nameFile: string;
+}
+
+function PakageFile(props: PakageFiles) {
+  return (
+    <div className="pl-9 pr-1 hover:bg-borderDarck">
+      <button className="flex gap-1 items-center py-[2px]">
+        {props.IconName}
+        <p className="text-base pl-1">{props.nameFile}</p>
+      </button>
+    </div>
+  );
+}
+
+export default PakageFile;
