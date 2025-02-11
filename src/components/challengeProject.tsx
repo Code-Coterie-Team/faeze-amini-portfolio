09cf7bb4f993084ae59c11a8d8244b50b9a76f1a
faeze <faezeh.am4212@gmail.com>
2025-02-11 23:13:12 +0330

    createChallengeProject

---
+++ src/components/challengeProject.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import SectionSeparator from "./sectionSeparator";
+
+interface ChallengeItems {
+  descrption: string;
+  item01?: string;
+  item02?: string;
+  item03?: string;
+}
+
+function ChallengeProject({
+  descrption,
+  item01,
+  item02,
+  item03,
+}: ChallengeItems) {
+  return (
+    <div
+      id="challenge"
+      className="challenge px-14 py-6"
+    >
+      <SectionSeparator />
+      <h1 className="mt-8 mb-5 font-medium text-4xl text-gray-50 tracking-tight ">
+        Challenge
+      </h1>
+      <p className=" text-lg">
+        {descrption}
+      </p>
+
+      <ul className="pl-10 list-disc text-lg">
+        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
+         {item01}
+        </li>
+        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
+          {item02}
+        </li>
+
+        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
+         {item03}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ChallengeProject;
