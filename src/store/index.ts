01f6d8ff5ad4a0eea22675ba6b2e0318584d8c72
faeze <faezeh.am4212@gmail.com>
2025-02-13 00:47:15 +0330

    addTabStorAndAddUuid

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,61 @@
-import { create } from 'zustand'
-
+import { ReactNode } from "react";
+import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
+import StarIcon from "../components/icons/StarIcon";
 interface StoreState {
-    sideBarShow: boolean;
-    toggleSideBarShow: () => void;
-    
-    projectName: string;
-    changeProjectName: (newName: string) => void;
-  }
-  
+  sideBarShow: boolean;
+  toggleSideBarShow: () => void;
 
+  projectName: string;
+  changeProjectName: (newName: string) => void;
+}
+
+interface ListTabs {
+  id: string;
+  tabTitle: string;
+  iconTab: ReactNode;
+  href:string;
+}
+
+interface TabStore {
+  tab: ListTabs[];
+  addTab: (tabTitle: string, iconTab: ReactNode, href:string) => void;
+}
 export const useStore = create<StoreState>((set) => ({
-    sideBarShow: false,
-    toggleSideBarShow:() => set((state) => ({ sideBarShow: !state.sideBarShow })),
- 
-    projectName: "adminDashboard",
-    changeProjectName: (newName) => set((state) => ({ projectName: newName })),
-}))
+  sideBarShow: false,
+  toggleSideBarShow: () =>
+    set((state) => ({ sideBarShow: !state.sideBarShow })),
 
+  projectName: "adminDashboard",
+  changeProjectName: (newName) => set((state) => ({ projectName: newName })),
+
+  listTab: [],
+}));
+// const initialTabs: ListTabs[] = [
+//   {
+//     id: uuidv4(),
+//     tabTitle: "About Me",
+//     iconTab: (
+//       <StarIcon
+//         width="18"
+//         height="18"
+//       />
+//     ),
+//   },
+// ];
+export const addTabStore = create<TabStore>((set) => ({
+  tab: [],
+  addTab: (tabTitle, iconTab,href) =>
+    set((state) => {
+      const exists = state.tab.some((tab) => tab.tabTitle === tabTitle);
+      if (exists) {
+        // console.warn(`Tab with title "${tabTitle}" already exists!`);
+        return state; // بازگشت به حالت قبلی بدون تغییر
+      }
+
+      const newtabId = uuidv4();
+      return {
+        tab: [...state.tab, { id: newtabId, tabTitle, iconTab,href }],
+      };
+    }),
+}));
