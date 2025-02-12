ee26af756a651520d583dfe3447626873c4c05a1
faeze <faezeh.am4212@gmail.com>
2025-02-13 02:34:38 +0330

    clgForShowTab

--- src/components/headerMain.tsx
+++ src/components/headerMain.tsx
@@ -9,21 +9,23 @@ import { addTabStore } from "@/store";
 
 function HeaderMain() {
   const { tab, addTab } = addTabStore();
-
+  console.log(tab, "tabList");
   useEffect(() => {
     addTab(
       "About Me",
       <StarIcon
         width="18"
         height="18"
-      />,"/"
+      />,
+      "/"
     );
   }, []);
   return (
     <div className="headerMain flex items-center justify-between w-full border-borderDarck border-b-2 bg-darckBg">
       <div className="flex overflow-hidden ">
         {tab.map((itemTab, index) => (
-          <AddNewTab key={index}
+          <AddNewTab
+            key={index}
             tabTitle={itemTab.tabTitle}
             iconTab={itemTab.iconTab}
             id={itemTab.id}
@@ -38,20 +40,6 @@ function HeaderMain() {
           />
 
           <p className="text-base">About Me</p>
-          <button className="p-1 hover:bg-gray-500/30 rounded-md">
-            <RemoveIcon
-              width="16"
-              height="16"
-            />
-          </button>
-        </div> */}
-        {/* <div className="title flex items-center gap-2 border-r-borderDarck border-b-blue-600 border-r-2 p-2">
-          <StarIcon
-            width="18"
-            height="18"
-          />
-
-          <p className="text-base">Work</p>
           <button className="p-1 hover:bg-gray-500/30 rounded-md">
             <RemoveIcon
               width="16"
