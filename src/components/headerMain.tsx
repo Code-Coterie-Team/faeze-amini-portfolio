b3e17ba57703e87092465a86e84a988df26a669a
faeze <faezeh.am4212@gmail.com>
2025-02-13 00:42:59 +0330

    addTabToHeaderMain

--- src/components/headerMain.tsx
+++ src/components/headerMain.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Layout10Icon from "./icons/Layout10Icon";
 import MenuCircleVerticalIcon from "./icons/MenuCircleVerticalIcon";
 import OpenChangeIcon from "./icons/OpenChangeIcon";
-import RemoveIcon from "./icons/RemoveIcon";
+import AddNewTab from "./addNewTab";
 import StarIcon from "./icons/StarIcon";
+import { addTabStore } from "@/store";
 
 function HeaderMain() {
+  const { tab, addTab } = addTabStore();
+
+  useEffect(() => {
+    addTab(
+      "About Me",
+      <StarIcon
+        width="18"
+        height="18"
+      />,"/"
+    );
+  }, []);
   return (
     <div className="headerMain flex items-center justify-between w-full border-borderDarck border-b-2 bg-darckBg">
-      <div className="flex">
-        <div className="title flex items-center gap-2 text-textActive border-r-borderDarck border-b-blue-600 border-r-2 border-b-2 p-2">
+      <div className="flex overflow-hidden ">
+        {tab.map((itemTab, index) => (
+          <AddNewTab key={index}
+            tabTitle={itemTab.tabTitle}
+            iconTab={itemTab.iconTab}
+            id={itemTab.id}
+            href={itemTab.href}
+          />
+        ))}
+
+        {/* <div className="title flex items-center gap-2 text-textActive border-r-borderDarck border-b-blue-600 border-r-2 border-b-2 p-2">
           <StarIcon
             width="18"
             height="18"
@@ -22,8 +44,8 @@ function HeaderMain() {
               height="16"
             />
           </button>
-        </div>
-        <div className="title flex items-center gap-2 border-r-borderDarck border-b-blue-600 border-r-2 p-2">
+        </div> */}
+        {/* <div className="title flex items-center gap-2 border-r-borderDarck border-b-blue-600 border-r-2 p-2">
           <StarIcon
             width="18"
             height="18"
@@ -36,10 +58,9 @@ function HeaderMain() {
               height="16"
             />
           </button>
-        </div>
+        </div> */}
       </div>
 
-    
       <div className="flex px-4">
         <button className="p-1 hover:bg-gray-500/30 rounded-md">
           <OpenChangeIcon
