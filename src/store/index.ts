6cb7fadf8ab359a8ab89e71d9b64061b1bf85d91
faeze <faezeh.am4212@gmail.com>
2025-02-17 21:54:38 +0330

    usePersistToLocalStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
-import { ReactNode } from "react";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { v4 as uuidv4 } from "uuid";
-import StarIcon from "../components/icons/StarIcon";
+
 interface StoreState {
   sideBarShow: boolean;
   toggleSideBarShow: () => void;
@@ -13,16 +13,16 @@ interface StoreState {
 interface ListTabs {
   id: string;
   tabTitle: string;
-  iconTab: ReactNode;
+
   href: string;
 }
 
 interface TabStore {
   tab: ListTabs[];
-  addTab: (tabTitle: string, iconTab: ReactNode, href: string) => void;
+  addTab: (tabTitle: string, href: string) => void;
   removeTab: (id: string) => void;
 
-  activeTab:string;
+  activeTab: string;
   changeActiveTab: (newLink: string) => void;
 }
 export const useStore = create<StoreState>((set) => ({
@@ -32,46 +32,50 @@ export const useStore = create<StoreState>((set) => ({
 
   projectName: "adminDashboard",
   changeProjectName: (newName) => set((state) => ({ projectName: newName })),
-
-  listTab: [],
 }));
-// const initialTabs: ListTabs[] = [
-//   {
-//     id: uuidv4(),
-//     tabTitle: "About Me",
-//     iconTab: (
-//       <StarIcon
-//         width="18"
-//         height="18"
-//       />
-//     ),
-//   },
-// ];
-export const addTabStore = create<TabStore>((set) => ({
-  tab: [],
-  addTab: (tabTitle, iconTab, href) =>
-    set((state) => {
-      const exists = state.tab.some((tab) => tab.tabTitle === tabTitle);
-      if (exists) {
-        // console.warn(`Tab with title "${tabTitle}" already exists!`);
-        return state;
-      }
+const initialTabs: ListTabs[] = [
+  {
+    id: uuidv4(),
+    tabTitle: "About Me",
+    href: "/",
+  },
+];
+export const addTabStore = create<TabStore>()(
+  persist((set, get) => ({
+    tab: initialTabs,
+    addTab: (tabTitle, href) =>
+      set((state) => {
+        const exists = state.tab.some((tab) => tab.tabTitle === tabTitle);
+        if (exists) {
+          // console.warn(`Tab with title "${tabTitle}" already exists!`);
+          return state;
+        }
 
-      const newtabId = uuidv4();
-      return {
-        tab: [...state.tab, { id: newtabId, tabTitle, iconTab, href }],
-      };
-    }),
-  removeTab: (id) =>
-    set((state) => {
-      if (state.tab.length === 1) {
-        return state;
-      }
-      return {
-        tab: state.tab.filter((tab) => tab.id !== id),
-      };
-    }),
+        const newtabId = uuidv4();
+        return {
+          tab: [...state.tab, { id: newtabId, tabTitle, href }],
+        };
+      }),
+    removeTab: (id) =>
+      set((state) => {
+        if (state.tab.length === 1) {
+          return state;
+        }
+        return {
+          tab: state.tab.filter((tab) => tab.id !== id),
+        };
+      }),
 
-    activeTab: "",
+    activeTab: "/",
     changeActiveTab: (newLink) => set((state) => ({ activeTab: newLink })),
-}));
+  }),
+
+  {
+    name: 'tabLink-storage', 
+    partialize: (state) => ({
+      tab: state.tab,
+      activeTab: state.activeTab,
+    
+    }),  },
+)
+);
