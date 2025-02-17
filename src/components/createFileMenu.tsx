fdd2fac47adb28d5b91d4a9381df177c93dcdc33
faeze <faezeh.am4212@gmail.com>
2025-02-17 21:47:03 +0330

    useChangeActiveTab

--- src/components/createFileMenu.tsx
+++ src/components/createFileMenu.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import React, { ReactNode } from "react";
-import AboutIcon from "./icons/AboutIcon";
+import { addTabStore, useStore } from "@/store";
 
 interface CreateFileMenus {
   href: string;
+  hrefPage: string;
   IconName: ReactNode;
   nameFile: string;
 }
 function CreateFileMenu(props: CreateFileMenus) {
+  const { changeActiveTab } = addTabStore();
   return (
-    <Link href={props.href}>
+    <Link onClick={() => (
+      changeActiveTab(props.hrefPage)
+    )} href={props.href}>
       <div className="flex items-center gap-2 pl-14 py-[2px]">
         {props.IconName}
         <p className="text-base">{props.nameFile}</p>
