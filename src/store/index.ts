5bbc00fda38902dff229f08d7c1f46c2963f08b6
faeze <faezeh.am4212@gmail.com>
2025-02-18 18:06:31 +0330

    usePersistLocalStorange

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { v4 as uuidv4 } from "uuid";
 
 interface StoreState {
@@ -24,6 +24,15 @@ interface TabStore {
 
   activeTab: string;
   changeActiveTab: (newLink: string) => void;
+
+  isPublicSideBar: boolean;
+  changeIsPublicSideBar: (isStatus: boolean) => void;
+
+  isSrcFolderSideBar: boolean;
+  changeSrcFolderSideBar: (isStatus: boolean) => void;
+
+  isMyFolderSideBar: boolean;
+  changeMyFolderSideBar: (isStatus: boolean) => void;
 }
 export const useStore = create<StoreState>((set) => ({
   sideBarShow: false,
@@ -41,41 +50,54 @@ const initialTabs: ListTabs[] = [
   },
 ];
 export const addTabStore = create<TabStore>()(
-  persist((set, get) => ({
-    tab: initialTabs,
-    addTab: (tabTitle, href) =>
-      set((state) => {
-        const exists = state.tab.some((tab) => tab.tabTitle === tabTitle);
-        if (exists) {
-          // console.warn(`Tab with title "${tabTitle}" already exists!`);
-          return state;
-        }
+  persist(
+    (set, get) => ({
+      tab: initialTabs,
+      addTab: (tabTitle, href) =>
+        set((state) => {
+          const exists = state.tab.some((tab) => tab.tabTitle === tabTitle);
+          if (exists) {
+            // console.warn(`Tab with title "${tabTitle}" already exists!`);
+            return state;
+          }
 
-        const newtabId = uuidv4();
-        return {
-          tab: [...state.tab, { id: newtabId, tabTitle, href }],
-        };
+          const newtabId = uuidv4();
+          return {
+            tab: [...state.tab, { id: newtabId, tabTitle, href }],
+          };
+        }),
+      removeTab: (id) =>
+        set((state) => {
+          if (state.tab.length === 1) {
+            return state;
+          }
+          return {
+            tab: state.tab.filter((tab) => tab.id !== id),
+          };
+        }),
+
+      activeTab: "/",
+      changeActiveTab: (newLink) => set((state) => ({ activeTab: newLink })),
+
+      isPublicSideBar: true,
+      changeIsPublicSideBar: (isStatus) =>
+        set((state) => ({ isPublicSideBar: isStatus })),
+
+      isSrcFolderSideBar: true,
+      changeSrcFolderSideBar: (isStatus) =>
+        set((state) => ({ isSrcFolderSideBar: isStatus })),
+
+      isMyFolderSideBar: true,
+      changeMyFolderSideBar: (isStatus) =>
+        set((state) => ({ isMyFolderSideBar: isStatus })),
+    }),
+
+    {
+      name: "tabLink-storage",
+      partialize: (state) => ({
+        tab: state.tab,
+        activeTab: state.activeTab,
       }),
-    removeTab: (id) =>
-      set((state) => {
-        if (state.tab.length === 1) {
-          return state;
-        }
-        return {
-          tab: state.tab.filter((tab) => tab.id !== id),
-        };
-      }),
-
-    activeTab: "/",
-    changeActiveTab: (newLink) => set((state) => ({ activeTab: newLink })),
-  }),
-
-  {
-    name: 'tabLink-storage', 
-    partialize: (state) => ({
-      tab: state.tab,
-      activeTab: state.activeTab,
-    
-    }),  },
-)
+    }
+  )
 );
