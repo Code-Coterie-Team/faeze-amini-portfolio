9d5fe871aa33ff3f3799541e5a9addebd8a35c94
faeze <faezeh.am4212@gmail.com>
2025-02-21 00:23:11 +0330

    createModalShow

---
+++ src/components/modal/modalShow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface IShowModalStatus {
+  title: string;
+  status: string;
+}
+function ModalShow({ title, status }: IShowModalStatus) {
+  return (
+    <div className="absolute left-1/2">
+      <span
+        className={`${status === "successful" ? " bg-green-600" : " bg-red-600"} px-4 py-2 text-gray-50 text-base top-5 rounded-2xl `}
+      >
+        {title}
+      </span>
+    </div>
+  );
+}
+
+export default ModalShow;
