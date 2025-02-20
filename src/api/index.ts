1bdca50497b0cb35741bc203e5d2b1878ca07e8b
faeze <faezeh.am4212@gmail.com>
2025-02-21 00:19:18 +0330

    createFileApi

---
+++ src/api/index.ts
@@ -0,0 +1,26 @@
+import { addTabStore } from "@/store";
+import axios from "axios";
+
+
+// const { isShowModalSuccessful, setIsShowModalSuccessful } = addTabStore();
+export const sendFormData = async (formData: {
+  name: string;
+  email: string;
+  company: string;
+  message: string;
+}) => {
+  try {
+    const response = await axios.post(
+      "https://67b704c52bddacfb270d4c25.mockapi.io/api/contactme/message",
+      formData
+    );
+    console.log("Data sent successfully:", response.data);
+    // setIsShowModalSuccessful(true);
+    
+    // await sendEmail(formData);
+  } catch (error) {
+    console.error("Error sending data:", error);
+  }
+};
+
+
