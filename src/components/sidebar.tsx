a0ed6527bc6395aeed562ae73614e0b862f5271e
faeze <faezeh.am4212@gmail.com>
2025-02-21 16:51:56 +0330

    showOrCloseSidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import FileIcon from "./icons/FileIcon";
 import SearchIcon from "./icons/SearchIcon";
 import BranchIcon from "./icons/BranchIcon";
@@ -17,30 +17,54 @@ interface IActiveIconSidebars {
 const activeIconSidebar: IActiveIconSidebars[] = [
   {
     activeIcon: "file",
-    iconSidebarName: <FileIcon width="32" height="32" />,
+    iconSidebarName: (
+      <FileIcon
+        width="32"
+        height="32"
+      />
+    ),
   },
   {
     activeIcon: "search",
-    iconSidebarName: <SearchIcon width="32" height="32" />,
+    iconSidebarName: (
+      <SearchIcon
+        width="32"
+        height="32"
+      />
+    ),
   },
   {
     activeIcon: "branch",
-    iconSidebarName: <BranchIcon width="32" height="32" />,
+    iconSidebarName: (
+      <BranchIcon
+        width="32"
+        height="32"
+      />
+    ),
   },
   {
     activeIcon: "runTime",
-    iconSidebarName: <RunitemIcon width="32" height="32" />,
+    iconSidebarName: (
+      <RunitemIcon
+        width="32"
+        height="32"
+      />
+    ),
   },
   {
     activeIcon: "extentionitem",
-    iconSidebarName: <ExtentionitemIcon width="32" height="32" />,
+    iconSidebarName: (
+      <ExtentionitemIcon
+        width="32"
+        height="32"
+      />
+    ),
   },
 ];
+
 function Sidebar() {
   const [isActiveSidebar, setIsActiveSidebar] = useState("file");
-
-  const { sideBarShow } = useStore();
-
+  const { sideBarShow, toggleSideBarShow } = useStore();
   return (
     <div className={`sidebar flex w-full flex-1 overflow-hidden`}>
       <div className={`subSidebar flex flex-col  pb-10 justify-between`}>
@@ -49,7 +73,20 @@ function Sidebar() {
             <button
               key={index}
               onClick={() => {
-                setIsActiveSidebar(item.activeIcon);
+                if (sideBarShow == false) {
+                  setIsActiveSidebar(item.activeIcon);
+                  toggleSideBarShow(true);
+                } else if (
+                  sideBarShow == true &&
+                  isActiveSidebar === item.activeIcon
+                ) {
+                  toggleSideBarShow(false);
+                } else if (
+                  sideBarShow == true &&
+                  isActiveSidebar !== item.activeIcon
+                ) {
+                  setIsActiveSidebar(item.activeIcon);
+                }
               }}
               className={`p-3 text-gray-500 hover:text-tGrayAll  ${
                 isActiveSidebar === item.activeIcon && sideBarShow == true
@@ -64,21 +101,28 @@ function Sidebar() {
 
         <div className="partSetting flex flex-col">
           <button className="p-3 text-gray-500 hover:text-tGrayAll ">
-            <UserCircleIcon width="32" height="32" />
+            <UserCircleIcon
+              width="32"
+              height="32"
+            />
           </button>
           <button className="p-3 text-gray-500 hover:text-tGrayAll ">
-            <SettingIcon width="32" height="32" />
+            <SettingIcon
+              width="32"
+              height="32"
+            />
           </button>
         </div>
       </div>
 
       <div
-        className={`flex flex-col ${!sideBarShow && "hidden" } border-r border-r-borderDarck overflow-hidden`}
+        className={`flex flex-col ${
+          !sideBarShow && "hidden"
+        } border-r border-r-borderDarck overflow-hidden`}
       >
-         <div className="w-[18rem]">
-         {isActiveSidebar === "file" && <DetailSubSidebar />}
-         </div>
-       
+        <div className="w-[18rem]">
+          {isActiveSidebar === "file" && <DetailSubSidebar />}
+        </div>
       </div>
     </div>
   );
