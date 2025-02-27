01e88a955b1f035b31434ea12a077a53a5f54ebb
faeze <faezeh.am4212@gmail.com>
2025-02-28 00:46:32 +0330

    changeActiveHashUseAndHoverPortfolio

--- src/components/srcFolder.tsx
+++ src/components/srcFolder.tsx
@@ -10,7 +10,7 @@ import ChallengeIcon from "./icons/ChallengeIcon";
 import SolutionIcon from "./icons/SolutionIcon";
 import TechnologiesIcon from "./icons/TechnologiesIcon";
 import ReactJsIcon from "./icons/ReactJsIcon";
-import { addTabStore } from "@/store";
+import { addTabStore, useStore } from "@/store";
 import CreateFileMenu from "./createFileMenu";
 import StarIcon from "./icons/StarIcon";
 
@@ -22,25 +22,45 @@ const listMenuLinkProjecs: Record<
     {
       href: "/apps/adminDashboard/#about",
       hrefPage: "/apps/adminDashboard",
-      IconName: <AboutIcon width="18" height="18" />,
+      IconName: (
+        <AboutIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "About",
     },
     {
       href: "/apps/adminDashboard/#challenge",
       hrefPage: "/apps/adminDashboard",
-      IconName: <ChallengeIcon width="18" height="18" />,
+      IconName: (
+        <ChallengeIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Challenge",
     },
     {
       href: "/apps/adminDashboard/#solution",
       hrefPage: "/apps/adminDashboard",
-      IconName: <SolutionIcon width="18" height="18" />,
+      IconName: (
+        <SolutionIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "SolutionIcon",
     },
     {
       href: "/apps/adminDashboard/#technologies",
       hrefPage: "/apps/adminDashboard",
-      IconName: <TechnologiesIcon width="18" height="18" />,
+      IconName: (
+        <TechnologiesIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Technologies",
     },
   ],
@@ -48,25 +68,45 @@ const listMenuLinkProjecs: Record<
     {
       href: "/apps/sneakersCompany/#about",
       hrefPage: "/apps/sneakersCompany",
-      IconName: <AboutIcon width="18" height="18" />,
+      IconName: (
+        <AboutIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "About",
     },
     {
       href: "/apps/sneakersCompany/#challenge",
       hrefPage: "/apps/sneakersCompany",
-      IconName: <ChallengeIcon width="18" height="18" />,
+      IconName: (
+        <ChallengeIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Challenge",
     },
     {
       href: "/apps/sneakersCompany/#solution",
       hrefPage: "/apps/sneakersCompany",
-      IconName: <SolutionIcon width="18" height="18" />,
+      IconName: (
+        <SolutionIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "SolutionIcon",
     },
     {
       href: "/apps/sneakersCompany/#technologies",
       hrefPage: "/apps/sneakersCompany",
-      IconName: <TechnologiesIcon width="18" height="18" />,
+      IconName: (
+        <TechnologiesIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Technologies",
     },
   ],
@@ -74,31 +114,64 @@ const listMenuLinkProjecs: Record<
     {
       href: "/apps/rockPaperScissorsGame/#about",
       hrefPage: "/apps/rockPaperScissorsGame",
-      IconName: <AboutIcon width="18" height="18" />,
+      IconName: (
+        <AboutIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "About",
     },
     {
       href: "/apps/rockPaperScissorsGame/#challenge",
       hrefPage: "/apps/rockPaperScissorsGame",
-      IconName: <ChallengeIcon width="18" height="18" />,
+      IconName: (
+        <ChallengeIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Challenge",
     },
     {
       href: "/apps/rockPaperScissorsGame/#solution",
       hrefPage: "/apps/rockPaperScissorsGame",
-      IconName: <SolutionIcon width="18" height="18" />,
+      IconName: (
+        <SolutionIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "SolutionIcon",
     },
     {
       href: "/apps/rockPaperScissorsGame/#technologies",
       hrefPage: "/apps/rockPaperScissorsGame",
-      IconName: <TechnologiesIcon width="18" height="18" />,
+      IconName: (
+        <TechnologiesIcon
+          width="18"
+          height="18"
+        />
+      ),
       nameFile: "Technologies",
     },
   ],
 };
 
-const iconTab =[<ReactJsIcon width="18" height="18"/>,<ReactJsIcon width="18" height="18"/>,<FileWorkIcon width="18" height="18"/>]
+const iconTab = [
+  <ReactJsIcon
+    width="18"
+    height="18"
+  />,
+  <ReactJsIcon
+    width="18"
+    height="18"
+  />,
+  <FileWorkIcon
+    width="18"
+    height="18"
+  />,
+];
 function SrcFolder() {
   const {
     addTab,
@@ -109,10 +182,12 @@ function SrcFolder() {
     changeMyFolderSideBar,
     changeSrcFolderSideBar,
   } = addTabStore();
+
+  const { hoverPortfolio, changeActiveHash } = useStore();
   let titleProject = "";
 
   return (
-    <div className="src ">
+    <div className="src relative overflow-hidden">
       <button
         onClick={() => {
           changeSrcFolderSideBar(!isSrcFolderSideBar);
@@ -121,17 +196,28 @@ function SrcFolder() {
         className="flex gap-1 items-center py-[2px] w-full hover:bg-borderDarck"
       >
         {isSrcFolderSideBar ? (
-          <DirectionDown01Icon className="ml-4" width="18" height="18" />
+          <DirectionDown01Icon
+            className="ml-4"
+            width="18"
+            height="18"
+          />
         ) : (
-          <DirectionRight01Icon className="ml-4" width="18" height="18" />
+          <DirectionRight01Icon
+            className="ml-4"
+            width="18"
+            height="18"
+          />
         )}
-        <FileSrcIcon width="18" height="18" />
+        <FileSrcIcon
+          width="18"
+          height="18"
+        />
         <p className="text-base pl-1">src</p>
       </button>
 
       {isSrcFolderSideBar ? (
         <>
-          <div className="myWorkFolder">
+          <div className="myWorkFolder relative overflow-hidden">
             <button
               onClick={() => {
                 changeMyFolderSideBar(!isMyFolderSideBar);
@@ -139,19 +225,33 @@ function SrcFolder() {
               className="flex gap-1 items-center py-[2px] w-full hover:bg-borderDarck"
             >
               {isMyFolderSideBar ? (
-                <DirectionDown01Icon className="ml-8" width="18" height="18" />
+                <DirectionDown01Icon
+                  className="ml-8"
+                  width="18"
+                  height="18"
+                />
               ) : (
-                <DirectionRight01Icon className="ml-8" width="18" height="18" />
+                <DirectionRight01Icon
+                  className="ml-8"
+                  width="18"
+                  height="18"
+                />
               )}
 
-              <FileWorkIcon width="18" height="18" />
+              <FileWorkIcon
+                width="18"
+                height="18"
+              />
               <p className="text-base pl-1">my work</p>
             </button>
             {isMyFolderSideBar && (
               <>
                 {Object.entries(listMenuLinkProjecs).map(
                   ([project, listProject], index) => (
-                    <div key={index} className={`${project}`}>
+                    <div
+                      key={index}
+                      className={`${project}`}
+                    >
                       <Link
                         onClick={() => {
                           titleProject =
@@ -164,18 +264,25 @@ function SrcFolder() {
                               .replace(/([A-Z])/g, " $1")
                               .trim()
                               .slice(1);
-                            
-                          addTab(titleProject, "/apps/" + project,<StarIcon/>);
+
+                          addTab(
+                            titleProject,
+                            "/apps/" + project,
+                            <StarIcon />
+                          );
                           changeActiveTab(`/apps/${project}`);
-                       
+                          changeActiveHash(`/apps/${project}/#about`);
                         }}
                         href={`/apps/${project}`}
                       >
                         <div
                           key={index}
-                          className="hover:bg-borderDarck flex items-center gap-2 pl-10 py-[2px]"
+                          className="hover:bg-borderDarck flex items-center gap-2 pl-14 py-[2px]"
                         >
-                          <ReactJsIcon width="18" height="18" />
+                          <ReactJsIcon
+                            width="18"
+                            height="18"
+                          />
                           <p className="text-base">
                             {project
                               .replace(/([A-Z])/g, " $1")
@@ -191,38 +298,23 @@ function SrcFolder() {
                       </Link>
                       {activeTab === `/apps/${project}` && (
                         <>
-                          {listProject.map((item, index) => (
-                            <CreateFileMenu
-                              key={index}
-                              href={item.href}
-                              hrefPage={item.hrefPage}
-                              IconName={item.IconName}
-                              nameFile={item.nameFile}
-                            />
-                          ))}
+                          <CreateFileMenu listFileMenu={listProject} />
+
+                          <div className="lineBordMyFolde"></div>
                         </>
                       )}
                     </div>
                   )
                 )}
-
-                {/* <FileProject
-                  nameProject="Rock Paper Scissors Game"
-                  href="/apps/rockPaperScissorsGame"
-                  IconProject={
-                    <ReactJsIcon
-                      width="18"
-                      height="18"
-                    />
-                  }
-                /> */}
               </>
             )}
+            {hoverPortfolio && <div className="lineBordMyFolde"></div>}
           </div>
         </>
       ) : (
         ""
       )}
+      {hoverPortfolio && <div className="lineBordSrc"></div>}
     </div>
   );
 }
