8041a33b8e9a9fa691c51aaa823e2e1614009522
faeze <faezeh.am4212@gmail.com>
2025-02-28 00:36:10 +0330

    useActiveHashProjectANDMotionFramer

--- src/components/challengeProject.tsx
+++ src/components/challengeProject.tsx
@@ -1,11 +1,15 @@
+"use client";
 import React from "react";
 import SectionSeparator from "./sectionSeparator";
+import { motion } from "framer-motion";
+import { useStore } from "@/store";
 
 interface ChallengeItems {
   descrption: string;
   item01?: string;
   item02?: string;
   item03?: string;
+  activeHashProject: string;
 }
 
 function ChallengeProject({
@@ -13,33 +17,48 @@ function ChallengeProject({
   item01,
   item02,
   item03,
+  activeHashProject,
 }: ChallengeItems) {
+  const { changeActiveHash } = useStore();
   return (
-    <div
+    <motion.div
       id="challenge"
       className="challenge md:px-14 px-2 py-6"
+      onViewportEnter={() =>
+        changeActiveHash(`/apps/${activeHashProject}/#challenge`)
+      }
     >
       <SectionSeparator />
       <h1 className="mt-8 mb-5 font-medium text-4xl text-gray-50 tracking-tight ">
         Challenge
       </h1>
-      <p className=" text-lg">
-        {descrption}
-      </p>
+      <p className=" text-lg">{descrption}</p>
 
       <ul className="pl-10 list-disc text-lg">
-        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
-         {item01}
+        <li
+          className={`mt-5 ${
+            item03 === "" || item03 === undefined ? "hidden" : ""
+          }`}
+        >
+          {item01}
         </li>
-        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
+        <li
+          className={`mt-5 ${
+            item03 === "" || item03 === undefined ? "hidden" : ""
+          }`}
+        >
           {item02}
         </li>
 
-        <li className={`mt-5 ${item03 === "" || item03 === undefined ? "hidden" : ""}`}>
-         {item03}
+        <li
+          className={`mt-5 ${
+            item03 === "" || item03 === undefined ? "hidden" : ""
+          }`}
+        >
+          {item03}
         </li>
       </ul>
-    </div>
+    </motion.div>
   );
 }
 
