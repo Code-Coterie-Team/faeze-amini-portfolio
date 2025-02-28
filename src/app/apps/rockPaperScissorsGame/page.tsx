787b9825997352e924b8e8aadd687f2f961033dd
faeze <faezeh.am4212@gmail.com>
2025-02-28 12:51:24 +0330

    pageRockProject

--- src/app/apps/rockPaperScissorsGame/page.tsx
+++ src/app/apps/rockPaperScissorsGame/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>
-      rock Paper Scissors Game
+    <div className="h-full overflow-y-auto">
+      <div className="flex flex-col gap-9"></div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
